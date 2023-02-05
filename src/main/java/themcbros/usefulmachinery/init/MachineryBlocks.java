864869968bd6cf648704266d5c307a6f8e8724f2
TheMCCrazyMan <themccrazyman@mail.ch>
2023-02-05 20:44:57 +0100

    creative powercell no more stackable

--- src/main/java/themcbros/usefulmachinery/init/MachineryBlocks.java
+++ src/main/java/themcbros/usefulmachinery/init/MachineryBlocks.java
@@ -22,7 +22,7 @@ public class MachineryBlocks {
     public static final ItemObject<AbstractMachineBlock> LAVA_GENERATOR = BLOCKS.register("lava_generator", () -> new LavaGeneratorBlock(Block.Properties.of(Material.METAL).strength(2f).sound(SoundType.METAL).requiresCorrectToolForDrops()), MACHINE_BLOCK_ITEM);
     public static final ItemObject<AbstractMachineBlock> ELECTRIC_SMELTER = BLOCKS.register("electric_smelter", () -> new ElectricSmelterBlock(Block.Properties.of(Material.METAL).strength(2f).sound(SoundType.METAL).requiresCorrectToolForDrops()), MACHINE_BLOCK_ITEM);
     public static final ItemObject<AbstractMachineBlock> COMPACTOR = BLOCKS.register("compactor", () -> new CompactorBlock(Block.Properties.of(Material.METAL).strength(2f).sound(SoundType.METAL).requiresCorrectToolForDrops()), MACHINE_BLOCK_ITEM);
-    public static final ItemObject<CreativePowerCellBlock> CREATIVE_POWER_CELL = BLOCKS.register("creative_power_cell", () -> new CreativePowerCellBlock(Block.Properties.of(Material.DECORATION).strength(.5f).sound(SoundType.METAL).noLootTable()), block -> new CreativePowerCellItem(block, PROPS));
+    public static final ItemObject<CreativePowerCellBlock> CREATIVE_POWER_CELL = BLOCKS.register("creative_power_cell", () -> new CreativePowerCellBlock(Block.Properties.of(Material.DECORATION).strength(.5f).sound(SoundType.METAL).noLootTable()), block -> new CreativePowerCellItem(block, new Item.Properties().stacksTo(1)));
 
 
     protected static void init() {
